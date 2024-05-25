2207cfe4393cf85be0db4d13a8b13f75c2911691
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-05-25 07:37:04 -0500

    Add comments to unit tests

--- ZIPChallenge.UnitTests/CreditCalculatorUnitTests.cs
+++ ZIPChallenge.UnitTests/CreditCalculatorUnitTests.cs
@@ -17,40 +17,52 @@ public class CreditCalculatorUSnitTests
     [TestMethod]
     public void CalculateCredit_WithLowBureauScore_ReturnsExpectedCredit()
     {
+        // Arrange
         var customer = new Customer(450, 0, 0, 30);
 
+        // Act
         var result = _creditCalculator.CalculateCredit(customer);
 
+        // Assert
         Assert.AreEqual(0, result);
     }
 
     [TestMethod]
     public void CalculateCredit_WithHighBureauScoreAndPayments_ReturnsExpectedCredit()
     {
+        // Arrange
         var customer = new Customer(800, 0, 2, 40);
 
+        // Act
         var result = _creditCalculator.CalculateCredit(customer);
 
+        // Assert
         Assert.AreEqual(500, result);
     }
 
     [TestMethod]
     public void CalculateCredit_WithMissedPayments_ReturnsZero()
     {
+        // Arrange
         var customer = new Customer(700, 2, 1, 50);
 
+        // Act
         var result = _creditCalculator.CalculateCredit(customer);
 
+        // Assert
         Assert.AreEqual(0, result);
     }
 
     [TestMethod]
     public void CalculateCredit_ExceedsMaxPointsByAge_ReturnsMaxCreditForAge()
     {
+        // Arrange
         var customer = new Customer(900, 0, 3, 20);
 
+        // Act
         var result = _creditCalculator.CalculateCredit(customer);
 
+        // Assert
         Assert.AreEqual(300, result);
     }
 }
